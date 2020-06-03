7963d70f57d0906d78972303954e40b558247433
Solid State Society <SolidStateSociety.src@gmail.com>
2020-06-03 15:46:59 -0400

    Add files via upload

---
+++ Lessons/Lesson 1/Lesson 1.1/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+/* header file for recognizing input and output */
+#include <iostream>
+
+/* makes sure compiler knows we're using the std lib */
+using namespace std;
+
+/* main() function
+* where the program execution begins
+*/
+int main()
+{
+	/* could also put 'std::' before 'cout' */
+	cout << "Hello World!";  // prints line
+
+	return 0;  // returns result
+}
